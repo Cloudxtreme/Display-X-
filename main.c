63499a1428841ecac364ae05d70c47c3cfc2fbd0
EricTran <eqtran@ucsd.edu>
2014-10-11 14:55:57 -0700

    main.c start

---
+++ main.c
@@ -0,0 +1,12 @@
+/*
+ * Filename: main.c
+ * Author: Eric Tran
+ * Userid: cs30xjy
+ * Description: C main file that initializes all functions used.
+ * Date: 10/11/14
+ * Sources of Help: TODO: List all the people, books, websites, etc. that you
+ *                  used to help you write the code in this source file.
+ */
+
+
+
